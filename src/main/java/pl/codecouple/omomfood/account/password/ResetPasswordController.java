576b27916f70ebc7318dee960774dade1c4dd08c
kchrusciel <krzysztof.chrusciel@outlook.com>
2016-12-23 17:17:09 +0100

    Add reset password controller

--- src/main/java/pl/codecouple/omomfood/account/password/ResetPasswordController.java
+++ src/main/java/pl/codecouple/omomfood/account/password/ResetPasswordController.java
@@ -3,5 +3,16 @@ package pl.codecouple.omomfood.account.password;
 /**
  * Created by Krzysztof Chruściel.
  */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ResetPasswordController {
+
+    @GetMapping("/reset-password")
+    public String resetPassword(){
+        return "reset-password";
+    }
+
 }
