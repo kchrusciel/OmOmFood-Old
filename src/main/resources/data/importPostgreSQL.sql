acb406c5bec94fde61dc2457fd1ab5e347730aa3
kchrusciel <krzysztof.chrusciel@outlook.com>
2016-12-27 11:51:39 +0100

    Change import SQL for postgres

--- src/main/resources/data/importPostgreSQL.sql
+++ src/main/resources/data/importPostgreSQL.sql
@@ -14,17 +14,17 @@ create table UserConnection (userId varchar(255) not null,
   primary key (userId, providerId, providerUserId));
 create unique index UserConnectionRank on UserConnection(userId, providerId, rank);
 
-insert into "user" (id, confirmation_id, confirmation_status, email, first_name, last_name, newsletter_status, password_encrypted) values (null, null, true, 'test1@test.pl', 'Krzysztof', 'Chrusciel', FALSE, '$2a$10$wG.lPg3cbXg/yfL1ZEU.yODOKnl//gaoi9HKFe91Ov9UghipxEaK6');
-insert into "user" (id, confirmation_id, confirmation_status, email, first_name, last_name, newsletter_status, password_encrypted) values (null, null, true, 'test2@test.pl', 'Jan', 'Nowak', FALSE, '$2a$10$wG.lPg3cbXg/yfL1ZEU.yODOKnl//gaoi9HKFe91Ov9UghipxEaK6');
+insert into "user" (confirmation_id, confirmation_status, email, first_name, last_name, newsletter_status, password_encrypted) values (null, true, 'test1@test.pl', 'Krzysztof', 'Chrusciel', FALSE, '$2a$10$wG.lPg3cbXg/yfL1ZEU.yODOKnl//gaoi9HKFe91Ov9UghipxEaK6');
+insert into "user" (confirmation_id, confirmation_status, email, first_name, last_name, newsletter_status, password_encrypted) values (null, true, 'test2@test.pl', 'Jan', 'Nowak', FALSE, '$2a$10$wG.lPg3cbXg/yfL1ZEU.yODOKnl//gaoi9HKFe91Ov9UghipxEaK6');
 
 
 -- References
-insert into reference (id, about_id, author_id, reference_content) values (null, 2, 1, 'One ipsum dolor sit amet, consectetur adipiscing elit');
-insert into reference (id, about_id, author_id, reference_content) values (null, 2, 1, 'Two ipsum dolor sit amet, consectetur adipiscing elit');
+insert into reference (about_id, author_id, reference_content) values (2, 1, 'One ipsum dolor sit amet, consectetur adipiscing elit');
+insert into reference (about_id, author_id, reference_content) values (2, 1, 'Two ipsum dolor sit amet, consectetur adipiscing elit');
 
 -- Offers
-insert into offer (id, city, created_date, description, event_date, file_name, owner_id, phone_number, price, quantity, title) values (null, 'Sosnowiec',  to_date ('2016-12-20 21:49:09','YYYY-MM-DD HH24:MI:SS'), 'Opis', to_date('2016-12-20 21:49:09','YYYY-MM-DD HH24:MI:SS'), 'test.png', 1, '500-600-700', 50, 4, 'Title');
+insert into offer (city, created_date, description, event_date, file_name, owner_id, phone_number, price, quantity, title) values ('Sosnowiec',  to_date ('2016-12-20 21:49:09','YYYY-MM-DD HH24:MI:SS'), 'Opis', to_date('2016-12-20 21:49:09','YYYY-MM-DD HH24:MI:SS'), 'test.png', 1, '500-600-700', 50, 4, 'Title');
 
 -- Messages
-insert into message (id, content, creation_date, is_read, offerid, owner, title) values (null, 'One ipsum dolor sit amet, consectetur adipiscing elit', to_date ('2016-12-20 21:49:09','YYYY-MM-DD HH24:MI:SS'), FALSE, 1, 1, 'title');
-insert into message (id, content, creation_date, is_read, offerid, owner, title) values (null, 'One ipsum dolor sit amet, consectetur adipiscing elit', to_date ('2016-12-20 21:49:09','YYYY-MM-DD HH24:MI:SS'), FALSE, 1, 1, 'title');
+insert into message (content, creation_date, is_read, offerid, owner, title) values ('One ipsum dolor sit amet, consectetur adipiscing elit', to_date ('2016-12-20 21:49:09','YYYY-MM-DD HH24:MI:SS'), FALSE, 1, 1, 'title');
+insert into message (content, creation_date, is_read, offerid, owner, title) values ('One ipsum dolor sit amet, consectetur adipiscing elit', to_date ('2016-12-20 21:49:09','YYYY-MM-DD HH24:MI:SS'), FALSE, 1, 1, 'title');
